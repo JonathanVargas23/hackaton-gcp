a4af63cace035147d30d96d1e96dbfc7d7639795
Jonathan <jonathanvargas32@gmail.com>
2022-07-08 10:49:42 -0500

    Add env for google cloud credentials

--- src/modules/hackaton/hackaton.service.ts
+++ src/modules/hackaton/hackaton.service.ts
@@ -7,20 +7,30 @@ export class HackatonService {
   // constructor() {}
   
   public async sendImage(data) {
+    const ENDPOINT_ID="629615542436626432";
+    const PROJECT_ID="hackaton-355614";
+    const INPUT_DATA_FILE="INPUT-JSON";
     // console.log(`funciona:`, data.buffer.toString());
-    console.log(`funciona:`, data);
+    console.log(`funciona:`, data.filename);
     
     const client = new PredictionServiceClient();
+    client.initialize();
     // Do something with DatasetServiceClient.
-    console.log('client', client);
+    // console.log('client', client);
+    
+    console.log(await client.getProjectId());
 
-    const request = {
-      endpoint: '',
-      instances: data,
+    const test_data = {
+      "endpoint": `https://us-central1-aiplatform.googleapis.com/v1/projects/${PROJECT_ID}/locations/us-central1/endpoints/${ENDPOINT_ID}:predict`,
+      "instances": [{
+        structValue: {
+          fields: data.filename
+        }
+      }]
     };
 
-    // const response = await client.explain(request);
-    // console.log('response', response);
+    const response = await client.predict(test_data);
+    console.log('response', response);
 
     return {
       message: `103 kl`
